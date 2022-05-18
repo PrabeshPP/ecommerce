2bf606bcc1fe18eaa657977bdaa010a86c93316c
Prabesh <prabesh.bista369@gmail.com>
2022-05-18 12:20:58 +0545

    SideNavbar Completed

--- src/Component/NavBar/Navbar.js
+++ src/Component/NavBar/Navbar.js
@@ -1,7 +1,47 @@
+import classes from "./Navbar.module.css";
+import {BiMenuAltLeft} from "react-icons/bi";
+import {SiHomeassistantcommunitystore} from "react-icons/si";
+import {FaShoppingBag} from "react-icons/fa";
+import {IoExit} from "react-icons/io5";
+import { useState } from "react";
+import {CgProfile} from "react-icons/cg";
+
+const ListIcons=[
+    BiMenuAltLeft,
+    SiHomeassistantcommunitystore,
+    FaShoppingBag,
+]
+
 function NavBar(){
+    const[isSignedIn,setSignedIn]=useState(true);
+
+    const onLogoutHandler=()=>{
+        setSignedIn(!isSignedIn);
+    }
+
     return(
-        <div>
-            
+        <div className={classes.nav}>
+            <div className={classes.menu}>
+            {
+                ListIcons.map((Icon)=>(
+                    <div className={classes.iconBox}>
+                    <Icon/>
+                    </div>
+                ))
+            }
+            </div>
+            <div className={classes.profile}>
+                {
+                    isSignedIn && <div className={classes.exit} onClick={onLogoutHandler}>
+                <IoExit/>
+                </div>
+                }
+                {
+                    !isSignedIn && <div className={classes.signIn} onClick={onLogoutHandler}>
+                <CgProfile/>
+                </div>
+                }
+            </div>
         </div>
     )
     
