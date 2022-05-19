5912a4c92ca60fdf4f972dda0b4e1873a726a00f
Prabesh <prabesh.bista369@gmail.com>
2022-05-19 21:05:34 +0545

    re-factor

--- src/Component/Body/ListItem.js
+++ src/Component/Body/ListItem.js
@@ -1,62 +1,62 @@
-import { useCallback, useEffect ,useState} from "react";
+import { useCallback, useEffect, useState } from "react";
 import classes from "./ListItem.module.css";
-import {FaCartPlus} from "react-icons/fa";
+import { FaCartPlus } from "react-icons/fa";
 
-const ListItem=()=>{
-    const[list,setList]=useState([]);
-    const[isLoading,setIsLoading]=useState(false);
-    const fetchData=useCallback(async()=>{
+const ListItem = () => {
+    const [list, setList] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
+    const fetchData = useCallback(async () => {
         setIsLoading(true);
-        try{
-            const response=await fetch("https://fakestoreapi.com/products");
-            if(!response.ok){
+        try {
+            const response = await fetch("https://fakestoreapi.com/products");
+            if (!response.ok) {
                 throw new Error("Could not fetch the Data");
             }
-            const data=await response.json();
-            const arrItems=[];
-           data.map((item)=>{
-               arrItems.push({
-                   id:item.id,
-                   image:item.image,
-                   price:item.price,
-                   title:item.title,
-                   description:item.description,
-                   category:item.category
-               })
-           })
+            const data = await response.json();
+            const arrItems = [];
+            data.map((item) => {
+                arrItems.push({
+                    id: item.id,
+                    image: item.image,
+                    price: item.price,
+                    title: item.title,
+                    description: item.description,
+                    category: item.category
+                })
+            })
 
-           setList(arrItems);
-           setIsLoading(false);
-        }catch(err){
+            setList(arrItems);
+            setIsLoading(false);
+        } catch (err) {
 
         }
-    },[]);
+    }, []);
 
-    useEffect(()=>{
+    useEffect(() => {
         fetchData();
-    },[fetchData])
+    }, [fetchData])
 
-    return(
+    return (
         <div className={classes.itemBox}>
-        {isLoading && <p>Loading....</p>}
-        {!isLoading && list.map((item)=>
-            <div className={classes.itemDiv} key={item.id}>
-                <img src={item.image} alt={item.title} className={classes.image}/>
-                <div className={classes.box}>
-                    <span className={classes.title}>{item.title}</span>
-                    <div className={classes.footer}>
-                    <span>${item.price}</span>
-                    <div className={classes.addtoCart}>
-                    <FaCartPlus className={classes.icon}/>
-                    </div>
+            {isLoading && <p>Loading....</p>}
+            {!isLoading && list.map((item) =>
+                <div className={classes.itemDiv} key={item.id}>
+                    <img src={item.image} alt={item.title} className={classes.image} />
+                    <div className={classes.box}>
+                        <span className={classes.title}>{item.title}</span>
+                        <div className={classes.footer}>
+                            <span>${item.price}</span>
+                            <div className={classes.addtoCart}>
+                                <FaCartPlus className={classes.icon} />
+                            </div>
+                        </div>
                     </div>
                 </div>
-            </div>
-        )}
-       
+            )}
+
         </div>
     )
-    
+
 
 }
 
