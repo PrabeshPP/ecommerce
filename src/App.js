4cea42f79705777983de785619311b2d825a02bf
Prabesh <prabesh.bista369@gmail.com>
2022-05-22 17:30:44 +0545

    addToCart Functionality added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
 import './App.css';
 import NavBar from './Component/NavBar/Navbar';
 import Body from './Component/Body/Body';
-import {useSelector,useDispatch} from "react-redux";
+
 function App() {
-  const dispatch=useDispatch();
-  const cart=useSelector((state)=>state.cart);
-  console.log(cart);
   return (
     <div className="App">
       <NavBar/>
--- src/Component/NavBar/Navbar.js
+++ src/Component/NavBar/Navbar.js
@@ -5,6 +5,7 @@ import {FaShoppingBag} from "react-icons/fa";
 import {IoExit} from "react-icons/io5";
 import { useState } from "react";
 import {CgProfile} from "react-icons/cg";
+import { useSelector } from "react-redux";
 
 const ListIcons=[
     BiMenuAltLeft,
@@ -13,6 +14,7 @@ const ListIcons=[
 ]
 
 function NavBar(){
+    const cart=useSelector((state)=>state.cart);
     const[isSignedIn,setSignedIn]=useState(true);
 
     const onLogoutHandler=()=>{
@@ -30,6 +32,7 @@ function NavBar(){
                     </div>
                     <div className={classes.cart}>
                     <FaShoppingBag/>
+                    <div className={classes.circle}>{cart.totalQuantity}</div>
                     </div>
             </div>
             <div className={classes.profile}>
