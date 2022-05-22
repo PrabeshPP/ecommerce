20ec741436c507a4594b48bc230ab125ed0ec4dc
Prabesh <prabesh.bista369@gmail.com>
2022-05-22 22:10:24 +0545

    cart page

--- src/Component/Cart/Cart.js
+++ src/Component/Cart/Cart.js
@@ -1,11 +1,28 @@
 import classes from "./Cart.module.css";
+import { useSelector } from "react-redux";
 
-const Cart=()=>{
-    return(
-        <div className={classes.bodyDiv}>
-
+const Cart = () => {
+  const cart = useSelector((state) => state.cart);
+  return (
+    <div className={classes.bodyDiv}>
+      <div className={classes.container}>
+        <div className={classes.header}>
+          <span className={classes.span1}>Shopping Cart</span>
+          <span className={classes.span2}>Deselect All Items</span>
+          <div className={classes.hr}></div>
         </div>
-    )
-}
+        <div className={classes.itemCard}>
+            <div className={classes.image}>
+
+            </div>
+            <div className={classes.desc}>
+
+            </div>
+        </div>
+      </div>
+      <div className={classes.checkout}></div>
+    </div>
+  );
+};
 
 export default Cart;
