0bc8a69e9c5fab9f2ffd3aab0baf3b93e64df71e
Prabesh <prabesh.bista369@gmail.com>
2022-05-23 19:46:26 +0545

    remove  from cart functionality added

--- src/Store/cartSlice.js
+++ src/Store/cartSlice.js
@@ -34,14 +34,14 @@ const CartSlice = createSlice({
 
     removeFromCart(state, action) {
       const item=state.items.find((item)=>item.id===action.payload);
-      if(item.quantity>1){
-        item.totalPrice=item.totalPrice-(item.totalPrice/item.quantity);
-        item.quantity--;
-        
+      state.totalQuantity--;
+      state.subTotal-=item.totalPrice/item.quantity;
+      state.changed=true;
+      if(item.quantity===1){
+        state.items=state.items.filter((item)=>item.id!==action.payload);
       }else{
-        console.log(action.payload)
-        const list=state.items.filter((item)=>item.id!==action.payload);
-        console.log(list);
+        item.totalPrice-=item.totalPrice/item.quantity
+        item.quantity--;
       }
     },
   },
